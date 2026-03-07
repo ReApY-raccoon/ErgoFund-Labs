1ff2ca52d4d7cda6d883e2dd42551764879178d4
aahilmomin21@gmail.com <aahilmak318@gmail.com>
2026-03-07 22:52:23 +0530

    docs: ergo index header comment

--- src/lib/ergo/index.ts
+++ src/lib/ergo/index.ts
@@ -1,6 +1,6 @@
 /**
- * Ergo blockchain integration for Bene Fundraising Platform
- * Fleet SDK + multi-wallet + multi-token support
+ * Ergo blockchain integration for Bene Fundraising Platform.
+ * Fleet SDK + multi-wallet + multi-token support.
  * For transactions: import { TransactionBuilder, OutputBuilder } from '@fleet-sdk/core'
  */
 
